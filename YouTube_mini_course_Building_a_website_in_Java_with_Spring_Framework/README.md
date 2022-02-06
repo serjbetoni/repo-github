2689bfbb2466a0fc022cfbec1a47e536320f9d81
Sergei Tsybakin <nord379@gmail.com>
2022-02-06 17:38:02 +0400

    add mini blog web site

---
+++ YouTube_mini_course_Building_a_website_in_Java_with_Spring_Framework/README.md
@@ -0,0 +1,10 @@
+# Building a website in Java
+  
+-I'm getting started with Spring Framework. This is my first tryout  
+-I watched the mini course: https://www.youtube.com/playlist?list=PL0lO_mIqDDFUYDRzvocu5EsFGBqPM7CIw  
+Creating a Web Blog: Adding, Outputting, Editing, Deleting Articles    
+-Spring project builder used: https://start.spring.io/    
+-HTML/CSS templates used: https://getbootstrap.com/    
+			https://www.bootstrapcdn.com/  
+  
+
---
+++ YouTube_mini_course_Building_a_website_in_Java_with_Spring_Framework/blog/src/main/java/com/example/blog/controllers/MainController.java
@@ -0,0 +1,21 @@
+package com.example.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "World");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "page about me");
+        return "about";
+    }
+}
